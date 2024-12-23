da817701c2447aa2483a81a82b79548959643537
mohamedalgafri <mohamed18540m@gmail.com>
2024-12-23 16:50:37 +0200

    up4

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -72,7 +72,7 @@ export function NavBar({ scroll = false, large = false, navItems }: NavBarProps)
         <div className="flex items-center gap-3 space-x-3">
 
           <Link
-            href={''}
+            href="/#donate"
             className="hidden md:block"
           >
             <Button
