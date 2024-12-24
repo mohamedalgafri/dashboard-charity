71f3bb153c0a6acd1d83a229ee81b37d2618b94d
mohamedalgafri <mohamed18540m@gmail.com>
2024-12-24 15:50:02 +0200

    up4

--- components/content/ProjectsGrid.tsx
+++ components/content/ProjectsGrid.tsx
@@ -20,28 +20,34 @@ export default function ProjectsGrid({ projects }: { projects: Project[] }) {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {projects.map((project) => (
           <Link href={`/projects/${project.slug}`} key={project.id}>
-            <Card className="hover:shadow-lg transition-shadow cursor-pointer h-full">
-              <div className="relative h-48 w-full">
-                <img
-                  src={project.coverImage || '/placeholder.jpg'} // إضافة صورة احتياطية
-                  alt={project.title}
-                  className="w-full h-full object-cover rounded-t-lg"
-                />
-              </div>
-              <CardHeader className="pb-0 pt-3 min-h-[85px]">
-                <CardTitle>{project.title}</CardTitle>
-                <CardDescription className="line-clamp-2 ">
-                  {project.description}
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <DonationProgress
+              <Card className="h-full overflow-hidden">
+                <div className="relative h-40">
+                  {project.coverImage ? (
+                    <img
+                      src={project.coverImage}
+                      alt={project.title}
+                      className="w-full h-full object-cover"
+                    />
+                  ) : (
+                    <div className="w-full h-full bg-gray-200" />
+                  )}
+                </div>
+                <CardHeader className="pb-0 pt-3 min-h-[85px]">
+                  <CardTitle className="text-lg line-clamp-1">
+                    {project.title}
+                  </CardTitle>
+                  <CardDescription className="line-clamp-2">
+                    {project.description}
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <DonationProgress
                     className="mt-0"
                     currentAmount={project.currentAmount}
                     targetAmount={project.targetAmount}
-                />
-              </CardContent>
-            </Card>
+                  />
+                </CardContent>
+              </Card>
           </Link>
         ))}
       </div>
