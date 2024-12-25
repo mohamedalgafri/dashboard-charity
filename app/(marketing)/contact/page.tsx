401c3e70c882773aad06768a2961f2c634a1f5d0
mohamedalgafri <mohamed18540m@gmail.com>
2024-12-25 13:56:41 +0200

    up4

--- app/(marketing)/contact/page.tsx
+++ app/(marketing)/contact/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
   return (
     <>
         <div className="mb-12 mt-12 text-center">
-            <h1 className="text-4xl font-bold mb-4">
+            <h1 className=" text-2xl md:text-4xl font-bold mb-4">
               تواصل معنا
             </h1>
             <p className="text-xl">
